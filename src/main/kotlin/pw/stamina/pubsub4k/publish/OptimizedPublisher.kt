86bb80bfaa18e4a416465a12dce1d07f9b8a4cbd
N3xuz <15078089+feature@users.noreply.github.com>
2019-02-14 18:47:43 +0100

    Add optimized publishers, and a publisher container.

---
+++ src/main/kotlin/pw/stamina/pubsub4k/publish/OptimizedPublisher.kt
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Stamina Development
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package pw.stamina.pubsub4k.publish
+
+import pw.stamina.pubsub4k.subscribe.Subscription
+
+sealed class OptimizedPublisher<T> : Publisher<T> {
+
+    companion object {
+        fun <T> fromSubscriptions(subscriptions: List<Subscription<T>>) = when (subscriptions.size) {
+            0 -> EmptyPublisher()
+            1 -> SingleSubscriptionPublisher(subscriptions[0])
+            else -> ManySubscriptionsPublisher(subscriptions)
+        }
+
+        fun <T> empty(): Publisher<T> {
+            return EmptyPublisher()
+        }
+    }
+}
+
+private class EmptyPublisher<T> : OptimizedPublisher<T>() {
+
+    override val subscriptions = emptyList<Subscription<T>>()
+
+    override fun publish(message: T) = Unit
+}
+
+private class SingleSubscriptionPublisher<T>(
+        subscription: Subscription<T>
+) : OptimizedPublisher<T>() {
+
+    private val messageHandler = subscription.messageHandler
+
+    override val subscriptions = listOf(subscription)
+
+    override fun publish(message: T) = messageHandler(message)
+}
+
+private class ManySubscriptionsPublisher<T>(
+        override val subscriptions: List<Subscription<T>>
+) : OptimizedPublisher<T>() {
+
+    private val messageHandlers = subscriptions.map(Subscription<T>::messageHandler)
+
+    override fun publish(message: T) = messageHandlers.forEach { handler -> handler(message) }
+}
---
+++ src/main/kotlin/pw/stamina/pubsub4k/publish/PublisherContainer.kt
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Stamina Development
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package pw.stamina.pubsub4k.publish
+
+import pw.stamina.pubsub4k.Topic
+import pw.stamina.pubsub4k.subscribe.Subscription
+
+/**
+ * A delegating implementation of the [Publisher] interface. This
+ * class is useful if
+ */
+class PublisherContainer<T>(
+        val topic: Topic<T>,
+        private var delegate: Publisher<T>,
+        private val factory: PublisherFactory
+) : Publisher<T> {
+
+    override val subscriptions: List<Subscription<T>>
+        get() = delegate.subscriptions
+
+    override fun publish(message: T) {
+        delegate.publish(message)
+    }
+
+    fun updateSubscriptions(subscriptions: List<Subscription<T>>) {
+        delegate = factory.createPublisher(subscriptions)
+    }
+
+    // add, remove, clear?
+}
