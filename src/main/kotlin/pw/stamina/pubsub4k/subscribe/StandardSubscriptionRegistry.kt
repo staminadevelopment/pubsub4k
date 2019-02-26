84c2d2427e0d19ba873e2d3c372e9df1212fd3a3
N3xuz <15078089+feature@users.noreply.github.com>
2019-02-26 10:57:58 +0100

    Add standard implementation of SubscriptionRegistry

---
+++ src/main/kotlin/pw/stamina/pubsub4k/subscribe/StandardSubscriptionRegistry.kt
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Stamina Development
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package pw.stamina.pubsub4k.subscribe
+
+import pw.stamina.pubsub4k.MessageSubscriber
+import pw.stamina.pubsub4k.Topic
+
+class StandardSubscriptionRegistry : SubscriptionRegistry {
+
+    private val subscriberToSubscriptionMap = mutableMapOf<MessageSubscriber, MutableSet<Subscription<*>>>()
+
+    override fun register(subscription: Subscription<*>) =
+            subscriberToSubscriptionMap.getOrPut(subscription.subscriber, ::mutableSetOf).add(subscription)
+
+    override fun registerAll(subscriptions: Set<Subscription<*>>): Set<Subscription<*>> {
+        return subscriptions.filterTo(mutableSetOf(), this::register)
+    }
+
+    override fun unregister(subscription: Subscription<*>): Boolean {
+        return subscriberToSubscriptionMap[subscription.subscriber]?.remove(subscription) ?: false
+    }
+
+    override fun unregisterAll(subscriber: MessageSubscriber): Set<Subscription<*>> {
+        return subscriberToSubscriptionMap.remove(subscriber) ?: emptySet()
+    }
+
+    override fun <T> findSubscriptionsForTopic(topic: Topic<T>): Set<Subscription<T>> {
+        return subscriberToSubscriptionMap.values.asSequence().flatten()
+                .filter { topic.isAssignableFrom(it.topic) }
+                .filterIsInstance<Subscription<T>>()
+                .filterTo(mutableSetOf()) {
+                    it.topicFilter?.accepts(topic) != false
+                }
+    }
+}
