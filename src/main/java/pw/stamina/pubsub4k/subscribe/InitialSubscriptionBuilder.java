f40827badde02d1ab3929f60ecc4a78292c33195
N3xuz <15078089+feature@users.noreply.github.com>
2019-03-27 00:27:01 +0100

    Switch to Apache License, Version 2.0

--- src/main/java/pw/stamina/pubsub4k/subscribe/InitialSubscriptionBuilder.java
+++ src/main/java/pw/stamina/pubsub4k/subscribe/InitialSubscriptionBuilder.java
@@ -1,25 +1,17 @@
 /*
- * MIT License
+ * Copyright 2019 Stamina Development
  *
- * Copyright (c) 2019 Stamina Development
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package pw.stamina.pubsub4k.subscribe;
@@ -33,9 +25,12 @@ import java.util.function.Predicate;
 
 public final class InitialSubscriptionBuilder<T> extends SubscriptionBuilder<T, T> {
 
-    @NotNull private final Class<T> topic;
-    @NotNull private final MessageSubscriber subscriber;
-    @Nullable private Predicate<Class<? extends T>> topicFilter;
+    @NotNull
+    private final Class<T> topic;
+    @NotNull
+    private final MessageSubscriber subscriber;
+    @Nullable
+    private Predicate<Class<? extends T>> topicFilter;
 
     InitialSubscriptionBuilder(@NotNull Class<T> topic, @NotNull MessageSubscriber subscriber) {
         this.topic = topic;
